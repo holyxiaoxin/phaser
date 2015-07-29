a9e4edd0a9ec8a2814a61b85323eda55fdc3e3bd
Jia Rong and Wei Jie <jr.loves.maths@gmail.com>
2015-07-30 01:46:10 +0800

    Trying to load existing players. But not so easy.

--- app.js
+++ app.js
@@ -8,14 +8,23 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + '/views/index.html');
 });
 
+var players = {};
+
 io.on('connection', function(socket){
     socket.on('new player', function(newPlayer){
+        players[newPlayer] = {};
         io.emit('new player', newPlayer);
-    });
-    socket.on('player position', function(object){
-        io.emit('player position', object);
+        socket.emit('new game', players);
     });
     socket.on('player action', function(object){
+        //console.log(players)
+        //console.log(object.name);
+        if(players[object.name]){
+            players[object.name].cursor = object.cursor;
+            players[object.name].cursorUp = object.cursorUp;
+            players[object.name].positionX = object.x;
+            players[object.name].positionY = object.y;
+        }
         io.emit('player action', object);
     });
 });
